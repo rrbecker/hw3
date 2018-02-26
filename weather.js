4acccdeb32613dcb4f42efedf4fc9e872d9f891e
rrbecker <35316419+rrbecker@users.noreply.github.com>
2018-02-26 12:45:27 -0600

    Initial commit

---
+++ weather.js
@@ -0,0 +1,32 @@
+let updateWidget = function(data) {
+
+  console.log("Got weather data: ", data)
+  // YOUR CODE GOES HERE
+
+  // HINT:
+  // Weather icons are provided for you. Sample URL: http://openweathermap.org/img/w/01d.png
+  // The very last part ('01d.png') should be obtained from the weather information.
+
+}
+
+
+let getWeather = function(event) {
+  let latitude = '48.8566';
+  let longitude = '2.3522';
+  let apiKey = '4ce6f502d38ddae567bf1702b05e168c'; // REPLACE THIS VALUE with your own key.
+
+  let weatherServiceURL = 'https://api.openweathermap.org/data/2.5/weather?'
+  weatherServiceURL += 'lat=' + latitude
+  weatherServiceURL += '&lon=' + longitude
+  weatherServiceURL +='&appid=' + apiKey + '&units=imperial'
+
+  fetch(weatherServiceURL).then(convertToJSON).then(updateWidget).catch(displayError);
+}
+
+
+////////////////////////////////////////////////////////////
+// YOU DO NOT NEED TO CHANGE ANY CODE BEYOND THIS POINT
+////////////////////////////////////////////////////////////
+
+let convertToJSON = function(rawData) { return rawData.json(); }
+let displayError = function(error) { console.debug(error); }
